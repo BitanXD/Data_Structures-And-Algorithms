38a32754f2a391890e6750bd91ec2902bb6235b2
BitanXD <bitansarkar088@gmail.com>
2024-08-23 03:27:17 +0530

    spiral matrix problem done

--- src/Matrix/SpiralMatrix.java
+++ src/Matrix/SpiralMatrix.java
@@ -6,11 +6,30 @@ public class SpiralMatrix {
     public void printSpiral(int[][] matrix, int row, int column){
         int i, k = 0, l = 0;
         while(k < row && l < column){
-            for(i = )
+            for(i = l; i < column; i++){  // left to right
+                System.out.print(matrix[k][i] + " ");
+            }
+            k++;
+            for(i = k; i < row; i++){   // top to bottom
+                System.out.print(matrix[i][column-1] + " ");
+            }
+            column--;
+            if(k < row){
+                for(i = column - 1; i >= l; i--){   // right to left
+                    System.out.print(matrix[row - 1][i] + " ");
+                }
+                row--;
+            }
+            if(l < column){
+                for(i = row - 1; i >= k; i--){   // bottom to top
+                    System.out.print(matrix[i][l] + " ");
+                }
+                l++;
+            }
         }
     }
 
-    public static void main(String[] arge){
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
         int[][] matrix = {
